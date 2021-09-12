30652737467846751b97183b9e9a5583576894c5
Vira Sukhanova <verusia.s@gmail.com>
2021-09-12 15:56:41 -0500

    advanced cypress progress

--- cypress-advanced/cypress/integration/intercept_request_start.js
+++ cypress-advanced/cypress/integration/intercept_request_start.js
@@ -2,8 +2,39 @@
 
 it('Intercept requests', () => {
 
+  // cy.
+  // intercept({
+  //   method: 'GET',
+  //   url: '/api/boards'
+  // }).as('boardList')
+
+  cy.
+  intercept({
+    method: 'POST',
+    url: '/api/boards'
+  }).as('createBoard')
+
+
   cy
     .visit('/')
+
+  cy
+  .get('[data-cy=create-board]')
+  .click()
+
+  cy
+  .get('[data-cy=new-board-input]')
+  .type('launching a rocket{enter}')
+
+  // cy.wait('@boardList')
+  // .its('response.statusCode')
+  // .should('eq', 200)
+
+  cy.wait('@createBoard')
+  .then((board) =>{
+    expect(board.response.statusCode).to.eq(201)
+    expect(board.request.body.name).to.eq('launching a rocket')
+  })
 
   cy
     .get('[data-cy=board-item]')
--- cypress-advanced/cypress/integration/sending_requests_start.js
+++ cypress-advanced/cypress/integration/sending_requests_start.js
@@ -1,8 +1,36 @@
 /// <reference types="cypress" />
 
-it('Sending requests', () => {
+beforeEach( ()=>{
+  cy
+  .request({
+    method: 'POST',
+    url: '/api/reset'
+  })
+})
+
+it.skip('Sending requests', () => {
 
   cy
     .visit('/')
+
+    let createResponse
+    cy
+    .request('POST', '/api/boards', {name: 'board created by .request() command'})
+    .then(({body}) =>{
+      createResponse = body
+    })
+  cy
+    .request({
+      method: 'PATCH',
+      url: '/api/boards/22274306652',
+      body:{
+        name: 'test board'
+      }
+    })
+  cy
+  .request({
+    method: 'DELETE',
+    url: '/api/boards/22274306652'
+  })
 
 });
